b41c5f9d54310f502a228489c3f23ac849f24c6f
Jahvah <elijah15gallardo@gmail.com>
2025-11-01 20:10:25 +0800

    sprite image when moving is added

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,7 @@
 package main;
 
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,7 +11,7 @@ public class GamePanel extends JPanel implements Runnable{
     final int originalTileSize = 16; //16x16 tile size
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale; //48x48 tile size
+    public final int tileSize = originalTileSize * scale; //48x48 tile size
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; //768 pixels
@@ -22,6 +24,7 @@ public class GamePanel extends JPanel implements Runnable{
 
     KeyHandlers keyH = new KeyHandlers();
     Thread gameThread; //game clock
+    Player player = new Player(this,keyH);
 
     //set player default position
     int playerX = 100;
@@ -80,20 +83,8 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     public void update(){
-        if(keyH.upPressed == true){
-            playerY -= playerSpeed;
-        }
-        else if(keyH.downPressed == true){
-            playerY += playerSpeed;
-        }
-        else if(keyH.leftPressed == true) {
-            playerX -= playerSpeed;
-        }
-        else if(keyH.rightPressed == true){
-            playerX += playerSpeed;
-        }
 
-
+        player.update();
 
 
     }
@@ -103,8 +94,9 @@ public class GamePanel extends JPanel implements Runnable{
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D)g;
-        g2.setColor(Color.white);
-        g2.fillRect(playerX,playerY,tileSize,tileSize);
+        player.draw(g2);
+
+
         g2.dispose();
 
     }
